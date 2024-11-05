df0c8db271f2a7fdcee796aba2ddbed8f76b8fe2
hazimco <140416762+hazimco@users.noreply.github.com>
2024-11-05 15:18:06 +0100

    adding buttons area

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import InputArea from "./src/components/InputArea";
+import ButtonsArea from "./src/components/ButtonsArea";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <InputArea />
+      <ButtonsArea />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/ButtonsArea.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "black",
+  },
+});
+
+const ButtonsArea = () => {
+  return <View style={styles.container}></View>;
+};
+
+export default ButtonsArea;
