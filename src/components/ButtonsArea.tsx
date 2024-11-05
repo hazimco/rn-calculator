23f01fa4e2c0059d32360af2632cd59978434478
hazimco <140416762+hazimco@users.noreply.github.com>
2024-11-05 15:27:24 +0100

    making buttons area take up 3/4 of the space and input area 1/4

--- src/components/ButtonsArea.tsx
+++ src/components/ButtonsArea.tsx
@@ -2,7 +2,11 @@ import { StyleSheet, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
+    flex: 3,
     backgroundColor: "black",
+
+    borderWidth: 1,
+    borderColor: "pink",
   },
 });
 
--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -2,8 +2,12 @@ import { StyleSheet, TextInput } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     color: "white",
     backgroundColor: "black",
+
+    borderWidth: 1,
+    borderColor: "red",
   },
 });
 
