1d6ce7b6a18b446ade81fb820d6a7d15f72692a4
hazimco <140416762+hazimco@users.noreply.github.com>
2024-11-06 00:19:05 +0100

    rendering basic arithmetic operation buttons

--- src/components/BasicArithmeticOperations.tsx
+++ src/components/BasicArithmeticOperations.tsx
@@ -1,14 +1,24 @@
 import { StyleSheet, View } from "react-native";
+import Button from "./Button";
+
+const operations = ["÷", "×", "+", "-", "="];
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     borderWidth: 1,
     borderColor: "lime",
   },
 });
 
 const BasicArithmeticOperations = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      {operations.map((operation) => (
+        <Button key={operation} title={operation} backgroundColor={"orange"} />
+      ))}
+    </View>
+  );
 };
 
 export default BasicArithmeticOperations;
