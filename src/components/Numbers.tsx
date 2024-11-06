bb84580127ed7385022a195c5f72a2f9dddb547e
hazimco <140416762+hazimco@users.noreply.github.com>
2024-11-06 01:37:18 +0100

    rendering number buttons

--- src/components/Numbers.tsx
+++ src/components/Numbers.tsx
@@ -1,4 +1,10 @@
 import { StyleSheet, View } from "react-native";
+import Button from "./Button";
+
+const firstRow = [7, 8, 9];
+const secondRow = [4, 5, 6];
+const thirdRow = [1, 2, 3];
+const fourthRow = [0, ","];
 
 const styles = StyleSheet.create({
   container: {
@@ -8,7 +14,28 @@ const styles = StyleSheet.create({
 });
 
 const Numbers = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <ButtonRow rowList={firstRow} />
+      <ButtonRow rowList={secondRow} />
+      <ButtonRow rowList={thirdRow} />
+      <ButtonRow rowList={fourthRow} />
+    </View>
+  );
+};
+
+interface ButtonRowProps {
+  rowList: (number | string)[];
+}
+
+const ButtonRow = ({ rowList }: ButtonRowProps) => {
+  return (
+    <View>
+      {rowList.map((number) => (
+        <Button key={number} title={`${number}`} backgroundColor={"#404040"} />
+      ))}
+    </View>
+  );
 };
 
 export default Numbers;
