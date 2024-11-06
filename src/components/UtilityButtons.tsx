26d6711d9a1d22f5acc3011c8430f9b372e47d4b
hazimco <140416762+hazimco@users.noreply.github.com>
2024-11-06 02:04:45 +0100

    rendering utility buttons

--- src/components/UtilityButtons.tsx
+++ src/components/UtilityButtons.tsx
@@ -1,4 +1,7 @@
 import { StyleSheet, View } from "react-native";
+import Button from "./Button";
+
+const utilities = ["AC", "\u207A\u2215\u208B", "%"]; //\u207A\u2215\u208B is +/-
 
 const styles = StyleSheet.create({
   container: {
@@ -8,7 +11,18 @@ const styles = StyleSheet.create({
 });
 
 const UtilityButtons = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      {utilities.map((utility) => (
+        <Button
+          key={utility}
+          title={utility}
+          backgroundColor={"#a0a0a0"}
+          textColor={"black"}
+        />
+      ))}
+    </View>
+  );
 };
 
 export default UtilityButtons;
