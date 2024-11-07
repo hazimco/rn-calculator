3e7c80b6d0aef5cee2067cf52c2641711cb05a00
hazimco <140416762+hazimco@users.noreply.github.com>
2024-11-07 14:01:15 +0100

    styling button - setting width to be relative to the screen width - this makes the buttons fill the screen width

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,27 @@
-import { ColorValue, StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+  ColorValue,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+
+const BUTTON_MARGIN = 5;
+const BUTTON_WIDTH = Dimensions.get("window").width / 4 - BUTTON_MARGIN * 2; // dividing by 4 since we have four buttons horizontally (in portrait)
 
 const styles = StyleSheet.create({
   container: {
     borderWidth: 1,
     borderColor: "white",
+    borderRadius: 100,
+    justifyContent: "center",
+    margin: BUTTON_MARGIN,
+    width: BUTTON_WIDTH,
+    height: BUTTON_WIDTH, // since we want height to equal width
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 36,
   },
 });
 
@@ -15,8 +33,8 @@ interface Props {
 
 const Button = ({ title, backgroundColor, textColor = "white" }: Props) => {
   return (
-    <TouchableOpacity style={{ backgroundColor }}>
-      <Text style={{ color: textColor }}>{title}</Text>
+    <TouchableOpacity style={[{ backgroundColor }, styles.container]}>
+      <Text style={[{ color: textColor }, styles.text]}>{title}</Text>
     </TouchableOpacity>
   );
 };
