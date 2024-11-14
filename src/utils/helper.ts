622b6b981d74e361f769d1bf4b652e32a76f3d09
hazimco <140416762+hazimco@users.noreply.github.com>
2024-11-14 02:15:15 +0100

    refactor: moving code for operator case to its own function to make the switch case easier to read

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,6 +2,17 @@ import { Button, ButtonStyle } from "../types";
 
 const operators = ["÷", "×", "-", "+"];
 
+const handleOperatorButton = (currentInput: string[], button: Button) => {
+  if (currentInput.length === 0) return currentInput;
+
+  const lastInputCharacter = currentInput.at(-1) || "";
+  if (!operators.includes(lastInputCharacter)) {
+    return [...currentInput, button.title]; //add operator if last character is not an operator
+  } else {
+    return currentInput.with(-1, button.title); //replace operator if last character is an operator
+  }
+};
+
 const getInputAfterButtonPress = (currentInput: string[], button: Button) => {
   switch (button.title) {
     case "AC": {
@@ -19,23 +30,16 @@ const getInputAfterButtonPress = (currentInput: string[], button: Button) => {
     case ",": {
       return currentInput;
     }
-
-    default: {
-      if (button.style === ButtonStyle.NUMBER) {
-        return [...currentInput, button.title];
-      }
-
-      const lastInputCharacter = currentInput.at(-1) || "";
-
-      if (button.style === ButtonStyle.OPERATION && currentInput.length > 0) {
-        if (!operators.includes(lastInputCharacter)) {
-          return [...currentInput, button.title]; //add operator if last character is not an operator
-        } else {
-          return currentInput.with(-1, button.title); //replace operator if last character is an operator
-        }
-      }
-      return currentInput;
+  }
+  switch (button.style) {
+    case ButtonStyle.NUMBER: {
+      return [...currentInput, button.title];
     }
+    case ButtonStyle.OPERATION: {
+      return handleOperatorButton(currentInput, button);
+    }
+    default:
+      return currentInput;
   }
 };
 
