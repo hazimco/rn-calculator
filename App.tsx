e72750e87cfd345d239691d6d1ab8089dbfd6b07
hazimco <140416762+hazimco@users.noreply.github.com>
2024-11-19 16:49:04 +0100

    improvement: automatically scrolling down when inputting a character that ends up on a new line

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import InputArea from "./src/components/InputArea";
 import ButtonsArea from "./src/components/ButtonsArea";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "./src/types";
 import { getInputAfterButtonPress } from "./src/utils/helper";
 
 export default function App() {
   const [input, setInput] = useState<string[]>([]);
+
+  const scrollViewRef = useRef<ScrollView>(null);
 
   const handlePress = (button: Button) => {
     setInput(getInputAfterButtonPress(input, button));
@@ -15,7 +17,12 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
+      <ScrollView
+        ref={scrollViewRef}
+        onContentSizeChange={() => {
+          scrollViewRef.current?.scrollToEnd({ animated: true });
+        }}
+      >
         <InputArea text={input} />
       </ScrollView>
       <ButtonsArea onPress={handlePress} />
