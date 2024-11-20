fb535bb14af3377e8194c42fd3dfa7a176b2f7bc
hazimco <140416762+hazimco@users.noreply.github.com>
2024-11-20 16:28:32 +0100

    using unicode code points instead of operator characters directly to make it easier to see what character has been used - e.g. difficult to see difference between hyphen and minus, division and plus, multiplication and letter 'x' when reading in the code editor

--- src/components/ButtonsArea.tsx
+++ src/components/ButtonsArea.tsx
@@ -1,33 +1,33 @@
 import { StyleSheet, View } from "react-native";
 import ButtonRow from "./ButtonRow";
-import { Button, ButtonStyle } from "../types";
+import { Button, ButtonStyle, UNICODE } from "../types";
 
 const firstRow: Button[] = [
   { title: "AC", style: ButtonStyle.UTILITY },
-  { title: "\u207A\u2215\u208B", style: ButtonStyle.UTILITY },
+  { title: UNICODE.PLUS_SLASH_MINUS, style: ButtonStyle.UTILITY },
   { title: "%", style: ButtonStyle.UTILITY },
-  { title: "÷", style: ButtonStyle.OPERATOR },
+  { title: UNICODE.DIVISION_SIGN, style: ButtonStyle.OPERATOR },
 ];
 
 const secondRow: Button[] = [
   { title: "7", style: ButtonStyle.NUMBER },
   { title: "8", style: ButtonStyle.NUMBER },
   { title: "9", style: ButtonStyle.NUMBER },
-  { title: "×", style: ButtonStyle.OPERATOR },
+  { title: UNICODE.MULTIPLICATION_SIGN, style: ButtonStyle.OPERATOR },
 ];
 
 const thirdRow: Button[] = [
   { title: "4", style: ButtonStyle.NUMBER },
   { title: "5", style: ButtonStyle.NUMBER },
   { title: "6", style: ButtonStyle.NUMBER },
-  { title: "−", style: ButtonStyle.OPERATOR },
+  { title: UNICODE.MINUS_SIGN, style: ButtonStyle.OPERATOR },
 ];
 
 const fourthRow: Button[] = [
   { title: "1", style: ButtonStyle.NUMBER },
   { title: "2", style: ButtonStyle.NUMBER },
   { title: "3", style: ButtonStyle.NUMBER },
-  { title: "+", style: ButtonStyle.OPERATOR },
+  { title: UNICODE.PLUS_SIGN, style: ButtonStyle.OPERATOR },
 ];
 
 const fifthRow: Button[] = [
--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,14 @@ export enum ButtonStyle {
   NUMBER = "#404040",
   UTILITY = "#a0a0a0",
 }
+
+export enum UNICODE {
+  // OPERATORS
+  PLUS_SIGN = "\u002b",
+  MINUS_SIGN = "\u2212",
+  MULTIPLICATION_SIGN = "\u00D7",
+  DIVISION_SIGN = "\u00F7",
+
+  // OTHER
+  PLUS_SLASH_MINUS = "\u207A\u2215\u208B",
+}
